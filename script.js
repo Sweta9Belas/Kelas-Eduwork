480141c2734f85cf7e7124a8420e87f9c37322c5
Sweta9Belas <71574994+Sweta9Belas@users.noreply.github.com>
2022-03-20 15:18:25 +0800

    penambahan dan perbaikan script

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-var nama = prompt("masukkan nama anda:");
-alert('nama ada adalah' nama);
+var nama = prompt("Masukkan nama anda:");
+var angka = prompt("Masukkan nilai anda: ");
+
+function konversi(nama, angka) {
+  if (angka >= 101) {
+    alert("Rentan nilai dari 1-100. Masukkan nilai yang benar");
+  } else if (100 >= angka <= 90) {
+    alert("Selamat anda mendapat nilai A");
+  } else if (89 >= angka <= 70) {
+    alert("Nilai anda B");
+  } else {
+    alert("Nilai anda C");
+  }
+}
+
+konversi(nama, angka);
+alert;
